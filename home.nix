4df76212f6a3da84eb64ded8291fa5e0cde02aa5
phozzy <arthur.fayzullin@gmail.com>
2023-07-01 23:01:40 +0200

    Install nerdfonts

--- home.nix
+++ home.nix
@@ -27,6 +27,7 @@
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
     # # fonts?
     # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    pkgs.nerdfonts
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
