625ca1bf5a36b673de04cf5c020cfe087836f0ed
phozzy <arthur.fayzullin@gmail.com>
2023-07-18 21:18:42 +0200

    Implement super tab

--- home.nix
+++ home.nix
@@ -167,6 +167,11 @@
       };
       extraConfigLuaPre = ''
         vim.opt.shortmess:append({ W = true, I = true, c = true })
+        local has_words_before = function()
+          unpack = unpack or table.unpack
+          local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+          return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        end
       '';
       colorschemes.catppuccin = {
         enable = true;
@@ -276,6 +281,36 @@
                 select = true,
               })
             '';
+            "<Tab>" = {
+              modes = ["i" "s"];
+              action = ''
+                function(fallback)
+                  if cmp.visible() then
+                    cmp.select_next_item()
+                  elseif require("luasnip").expand_or_locally_jumpable() then
+                    require("luasnip").expand_or_jump()
+                  elseif has_words_before() then
+                    cmp.complete()
+                  else
+                    fallback()
+                  end
+                end
+              '';
+            };
+            "<S-Tab>" = {
+              modes = ["i" "s"];
+              action = ''
+                function(fallback)
+                  if cmp.visible() then
+                    cmp.select_prev_item()
+                  elseif require("luasnip").jumpable(-1) then
+                    require("luasnip").jump(-1)
+                  else
+                    fallback()
+                  end
+                end
+              '';
+            };
           };
           mappingPresets = [
             "insert"
