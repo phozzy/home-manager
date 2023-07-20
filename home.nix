d972de49b3fda55062cff1501681261ccf3004d7
phozzy <arthur.fayzullin@gmail.com>
2023-07-20 11:03:53 +0200

    Add a function to get root dir

--- home.nix
+++ home.nix
@@ -172,6 +172,40 @@
           local line, col = unpack(vim.api.nvim_win_get_cursor(0))
           return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
         end
+        local get_root = function()
+          ---@type string?
+          local path = vim.api.nvim_buf_get_name(0)
+          path = path ~= "" and vim.loop.fs_realpath(path) or nil
+          ---@type string[]
+          local roots = {}
+          if path then
+            for _, client in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+              local workspace = client.config.workspace_folders
+              local paths = workspace and vim.tbl_map(function(ws)
+                return vim.uri_to_fname(ws.uri)
+              end, workspace) or client.config.root_dir and { client.config.root_dir } or {}
+              for _, p in ipairs(paths) do
+                local r = vim.loop.fs_realpath(p)
+                if path:find(r, 1, true) then
+                  roots[#roots + 1] = r
+                end
+              end
+            end
+          end
+          table.sort(roots, function(a, b)
+            return #a > #b
+          end)
+          ---@type string?
+          local root = roots[1]
+          if not root then
+            path = path and vim.fs.dirname(path) or vim.loop.cwd()
+            ---@type string?
+            root = vim.fs.find(M.root_patterns, { path = path, upward = true })[1]
+            root = root and vim.fs.dirname(root) or vim.loop.cwd()
+          end
+          ---@cast root string
+          return root
+        end
       '';
       colorschemes.catppuccin = {
         enable = true;
